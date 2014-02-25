096b181a2098c698bb9c88ca996f72d89e6aa99c
theotheu <6611kw06>
2014-02-25 21:07:13 +0100

    heartbeat added

--- server.js
+++ server.js
@@ -13,9 +13,8 @@ io.sockets.on("connection", function (socket) {
     // Sends a message every 1000 milliseconds
     setInterval(function () {
         var d = new Date();
-        console.log('d', d, socket);
         io.sockets.emit('heartbeat', {hello: 'world!', date: d});
-    }, 1000);
+    }, 4000);
 
     // Sends a message to the chat board
     socket.on("sendMessage", function (data) {
