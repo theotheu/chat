143d794110fc737bee370d8df4bbfbe10cbf915f
Theo Theunissen <theo.theunissen@gmail.com>
2015-09-03 23:05:43 +0200

    checking in to work on laptop tomorrow

--- tests/e2e/test.js
+++ tests/e2e/test.js
@@ -5,6 +5,7 @@
  */
 
 var webdriverio = require('webdriverio');
+var assert = require('assert');
 var options = {
     desiredCapabilities: {
         /**
@@ -17,25 +18,108 @@ var options = {
          * @see http://elementalselenium.com/tips/69-safari
          *
          */
-        browserName: 'phantomjs' // phantomjs | chrome | firefox | safari
+        browserName: 'chrome' // phantomjs | chrome | firefox | safari
     }
 };
 
-webdriverio
-    .remote(options)
-    .init()
-    .url('http://localhost:3000')
-    .title(function(err, res) {
-        console.log('Title was: ' + res.value);
-    })
-    .waitForExist("#message", function(){
-        console.log('finally');
+//webdriverio
+//    .remote(options)
+//    .init()
+//    .url('http://localhost:3000')
+//    .title(function (err, res) {
+//        console.log('Title was: ' + res.value);
+//    })
+//    .waitForExist("#message", function () {
+//        console.log('finally');
+//
+//    })
+//    .setValue("#message", "tada!")
+//    .submitForm("#submitBtn")
+//    .end();
 
-    })
-    .setValue("#message", "tada!")
-    .submitForm("#submitBtn")
+describe("First simple test", function () {
+
+    this.timeout(99999999);
+    var client = {};
+
+    before(function (done) {
+        client = webdriverio.remote(options);
+        client.init(done);
+    });
 
 
+    it("Title should be 'Chat'", function (done) {
+        client
+            .url('http://localhost:3000')
+            .getTitle(function (err, title) {
+                assert(err === undefined);
+                assert(title === 'Chat');
+            })
+            .call(done);
+    });
 
 
-    .end();
+    it("Title should be 'Chat'", function (done) {
+        client
+            .url('http://localhost:3000')
+            .getTitle(function (err, title) {
+                assert(err === undefined);
+                assert(title === 'Chat');
+            })
+            .setValue("#message", "tada!")
+            .submitForm("#submitBtn")
+            .call(done);
+    });
+
+
+    after(function (done) {
+        client.end(done);
+    });
+
+});
+describe("Second test", function () {
+
+    this.timeout(99999999);
+    var client1 = {};
+    var client2 = {};
+
+    before(function (done) {
+        client1 = webdriverio.remote(options)
+        client2 = webdriverio.remote(options)
+
+
+        ;
+        client1.init(done);
+        client2.init(done);
+    });
+
+    it("Title should be 'Chat'", function (done) {
+        client1
+            .url('http://localhost:3000')
+            .setValue("#message", "tada! 2nd resr")
+            .submitForm("#submitBtn")
+            .then(function (err, val) {
+                console.log('>>>>>', err, val)
+            })
+
+            .call(done)
+        ;
+
+
+        //client2
+        //    .url('http://localhost:3000')
+        //    .getValue("#message", "tada! 2nd resr")
+        //     .then(function (err, val) {
+        //        console.log('>>>>>', err, val)
+        //    })
+        //
+        //    .call(done);
+    });
+
+
+    after(function (done) {
+        client1.end(done);
+        client2.end(done);
+    });
+
+});
