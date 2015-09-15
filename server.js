8f57d0fa48e10aaeb80c61a54fa62f7c610a0b89
Max van de Peppel <maxpeppel@hotmail.com>
2015-09-15 12:08:59 +0200

    chanhes added

--- server.js
+++ server.js
@@ -4,21 +4,26 @@ var express = require("express"),
     io = require("socket.io").listen(server);
 
 server.listen(3000);
+
 app.get("/", function (req, res) {
     res.sendfile(__dirname + "/index.html");
 });
 
 io.sockets.on("connection", function (socket) {
 
-    // Sends a message every 1000 milliseconds
-    setInterval(function () {
-        var d = new Date();
-        io.sockets.emit('heartbeat', {hello: 'world!', date: d});
-    }, 1000);
-
-    // Sends a message to the chat board
+	io.sockets.emit("newMessage", 'a user connected');
+	
+    socket.on('message', function(msg){
+		io.sockets.emit('message', msg);
+		msg = "Automated test message: " + msg;
+		io.sockets.emit("newMessage", msg);
+	});
     socket.on("sendMessage", function (data) {
         io.sockets.emit("newMessage", data);
     });
+	
+	socket.on('disconnect', function(){
+		io.sockets.emit("newMessage", 'a user disconnected');
+	});
 
 });
